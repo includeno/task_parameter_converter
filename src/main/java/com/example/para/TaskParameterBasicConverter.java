ee12d87c72bf7e2dddf565a285f6e89cc3b10023
includeno <includeno@gmail.com>
2023-07-10 10:10:23 +0800

    基础参数

--- src/main/java/com/example/para/TaskParameterBasicConverter.java
+++ src/main/java/com/example/para/TaskParameterBasicConverter.java
@@ -1,4 +1,23 @@
 package com.example.para;
 
-public class TaskParameterBasicConverter {
+import java.time.LocalDate;
+import java.util.function.Function;
+
+//task基础参数 类型转换器
+public enum TaskParameterBasicConverter implements TaskParameterConverterInterface {
+    date(LocalDate::parse, LocalDate.class),
+    ;
+
+    private final Function<String, ?> converter;
+    private final Class<?> clazz;
+
+    <T> TaskParameterBasicConverter(Function<String, T> converter, Class<T> clazz) {
+        this.converter = converter;
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Function<String, ?> getConverter() {
+        return converter;
+    }
 }
